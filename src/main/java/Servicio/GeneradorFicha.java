77a45c8127567c87a36cc6df503495901a647448
nelsonlatino03 <nlatino70804@ufide.ac.cr>
2026-07-23 23:27:23 -0600

    avance 2 proyecto final

--- src/main/java/Servicio/GeneradorFicha.java
+++ src/main/java/Servicio/GeneradorFicha.java
@@ -1,50 +1,45 @@
 package Servicio;
 
 /**
- * Gestiona las fichas del sistema hospitalario
- * @author ignap
+ * Genera fichas para pacientes regulares y preferenciales.
+ *
+ * @author nelson
  */
 public class GeneradorFicha {
-    
-    //Atributos
+
     private int contadorRegular;
     private int contadorPreferencial;
 
-    //Constructores
     /**
-     * Crea una ficha con los valores ingresados
-     * @param contadorRegular contador de pacientes Regulares
-     * @param contadorPreferencial contador de pacientes Preferenciales
+     * Constructor.
      */
-    public GeneradorFicha(int contadorRegular, int contadorPreferencial) {
-        this.contadorRegular = 0;
-        this.contadorPreferencial = 0;
-    }  
+    public GeneradorFicha() {
 
-    public GeneradorFicha() {}
-    
-    //Metodos
+        contadorRegular = 0;
+        contadorPreferencial = 0;
+    }
+
     /**
-     * Generar una ficha consecutiva para un paciente regular
-     * Cada vez que se invoca, incrementa el contador
-     * retornando una cadena con P + contador
-     * @return Numero de ficha para un paciente Regular
+     * Genera una ficha para un paciente regular.
+     *
+     * @return ficha regular
      */
     public String generarRegular() {
-        contadorRegular += 1;
-        
-        return "R" + contadorRegular;
+
+        contadorRegular++;
+
+        return "R-" + contadorRegular;
     }
-    
+
     /**
-     * Generar una ficha consecutiva para un paciente preferencial
-     * Cada vez que se invoca, incrementa el contador
-     * retornando una cadena con P + contador
-     * @return Numero de ficha para un paciente Preferencial
+     * Genera una ficha para un paciente preferencial.
+     *
+     * @return ficha preferencial
      */
-    public String generarPreferencial(){
-        contadorPreferencial += 1;
-        
-        return "P" + contadorPreferencial;
+    public String generarPreferencial() {
+
+        contadorPreferencial++;
+
+        return "P-" + contadorPreferencial;
     }
 }
